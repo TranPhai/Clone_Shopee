a3c2aeec3272a38a9ceff391d735f54453aac1d7
TranPhai <tranphai792@gmail.com>
2022-03-19 22:31:50 +0700

    update_data

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,45 +1,145 @@
 const modal = document.getElementsByClassName('modal');
 const comback = document.getElementsByClassName('auth-form__controls-back');
-const btnRegister = document.getElementById('auth-form__switch-btn-register')
-const btnLogin = document.getElementById('auth-form__switch-btn-login')
-const formRegister = document.getElementById('auth-form__register')
-const formLogin = document.getElementById('auth-form__login')
-const headerRegister = document.getElementById('header__navbar-item-register')
-const headerLogin = document.getElementById('header__navbar-item-login')
-const modal__overlay = document.getElementsByClassName('modal__overlay')
+const btnRegister_switch = document.getElementById('auth-form__switch-btn-register')
+const btnLogin_switch = document.getElementById('auth-form__switch-btn-login');
+const formRegister = document.getElementById('auth-form__register');
+const formLogin = document.getElementById('auth-form__login');
+const headerRegister = document.getElementById('header__navbar-item-register');
+const headerLogin = document.getElementById('header__navbar-item-login');
+const modal__overlay = document.getElementsByClassName('modal__overlay');
+
+const btnLogin = document.getElementById('btn-login') ;
+const btnRegister = document.getElementById('btn-register');
+
+const input_gmail_register = document.getElementById('auth-form__input-gmail--register');
+const input_pass_register = document.getElementById('auth-form__input-pass--register');
+const input_repass_register = document.getElementById('auth-form__input-repass--register');
+const input_name_register = document.getElementById('auth-form__input-name--register');
+
+const input_gmail_login = document.getElementById('auth-form__input-gmail--login');
+const input_pass_login = document.getElementById('auth-form__input-pass--login');
+
+const item_user = document.getElementById('header__navbar-item-user');
+
+//
+
+
 // like product
 const likeProduct = document.getElementsByClassName('home__product-item__like');
-modal[0].style.display = 'none';
+
 for(var i = 0; i < comback.length; i++){
     comback[i].addEventListener('click',function(){
         modal[0].style.display = 'none';
     });
 }
-
-btnLogin.onclick = function(){
+// event login_register
+btnLogin_switch.onclick = function(){
     formRegister.style.display ='none';
     formLogin.style.display ='block';
 }
-btnRegister.onclick = function(){
+btnRegister_switch.onclick = function(){
     formLogin.style.display ='none';
     formRegister.style.display ='block';
 }
-// headerLogin.onclick = function(){
-//     modal[0].style.display = 'flex';
-//     formRegister.style.display ='none';
-//     formLogin.style.display ='block';
-// }
-// headerRegister.onclick = function(){
-//     modal[0].style.display = 'flex';
-//     formLogin.style.display ='none';
-//     formRegister.style.display ='block';
-// }
+headerLogin.onclick = function(){
+    modal[0].style.display = 'flex';
+    formRegister.style.display ='none';
+    formLogin.style.display ='block';
+}
+headerRegister.onclick = function(){
+    modal[0].style.display = 'flex';
+    formLogin.style.display ='none';
+    formRegister.style.display ='block';
+}
 
 modal__overlay[0].onclick = function(){
     modal[0].style.display = 'none';
 
 }
-console.log(likeProduct);
 likeProduct[0].onclick = function(){
     likeProduct[0].classList.toggle('home__product-item__like--liked');
+}
+let gmail_register;
+let name_register;
+let pass_register;
+let repass_register;
+let gmail_login;
+let pass_login;
+let name_login;
+
+// register input
+input_gmail_register.onchange= (e) =>{
+    gmail_register = e.target.value;
+
+}
+input_name_register.onchange= (e) =>{
+    name_register = e.target.value;
+
+}
+input_pass_register.onchange= (e) =>{
+    pass_register = e.target.value;
+}
+input_repass_register.onchange= (e) =>{
+    repass_register = e.target.value;
+
+}
+
+// login input
+input_gmail_login.onchange= (e) =>{
+    gmail_login = e.target.value;
+
+}
+input_pass_login.onchange= (e) =>{
+    pass_login = e.target.value;
+
+}
+
+
+const accountArr = [
+
+]
+
+let accounts = JSON.parse(localStorage.getItem('accounts'))
+
+btnRegister.onclick = () =>{
+    if(pass_register === repass_register){
+        accountArr.push({gmail_register,name_register,pass_register})
+        localStorage.setItem('accounts',JSON.stringify(accountArr));
+    }
+    else if(pass_register !== repass_register){
+        console.log('Mật khẩu không khớp')
+    }
+    
+}   
+btnLogin.onclick = () =>{
+
+const user = accounts.map((account) =>{
+    if(account.gmail_register == gmail_login && account.pass_register == pass_login ){
+        alert('Susscess')
+        modal[0].style.display = 'none';
+    }
+    
+    return `
+    <li class="header__navbar-item header__navbar-user">
+        <img src="./assets/img/avatarNobody.png" alt=""
+            class="header__navbar-user-img">
+        <span class="header__navbar-user-name">${account.name_register}</span>
+        <ul class="header__navbar-user-menu">
+            <li class="header__navbar-user-item">
+                <a href="">Tài khoản của tôi</a>
+            </li>
+            <li class="header__navbar-user-item">
+                <a href="">Địa chỉ của tôi</a>
+            </li>
+            <li class="header__navbar-user-item">
+                <a href="">Đơn mua</a>
+            </li>
+            <li class="header__navbar-user-item header__navbar-user-item-separate">
+                <a href="">Đăng xuất</a>
+            </li>
+        </ul>
+    </li>
+    `
+  })
+  item_user.innerHTML = user.join('')
 }
---
+++ assets/js/view_product.js
@@ -0,0 +1,206 @@
+const home__product = document.getElementById('home__product-swap');
+const header_cart = document.getElementById('header__cart-list-item-id');
+const btnBuy = document.getElementById('home__product-buy-id')
+
+const items = [
+    {
+        name : 'Áo thun polo Nam',
+        image :'https://cf.shopee.vn/file/718d9c1f8bf671450d308e49b87d40a6',
+        price_old : 350000,
+        price_current : 200000,
+        solded : '100',
+        brand : 'TP',
+        address : 'Nam Định',
+       
+    },
+    {
+        name : 'Bộ Quần Áo Mặc Nhà Thể Thao Nam Mùa Hè Phong Cách Cao Cấp ZERO',
+        image :'https://cf.shopee.vn/file/1a32d71426b5299936d59909870e92b6',
+        price_old : 350000,
+        price_current : 250000,
+        solded : '100',
+        brand : 'TP',
+        address : 'Nam Định',
+       
+    },
+    {
+        name : 'Bộ Quần Áo Nam Thể Thao Viền Tay Cao Cấp thời Trang ZERO',
+        image :'https://cf.shopee.vn/file/1fb01bba24c1bf931ad47b6167ddb18d',
+        price_old : 250000,
+        price_current : 200000,
+        solded : '100',
+        brand : 'TP',
+        address : 'Nam Định',
+       
+    },
+    {
+        name : 'Quần âu nam Ikemen chất vải tuyết mưa ,co giãn 4 chiều dáng ôm body chống nhăn',
+        image :'https://cf.shopee.vn/file/d9085f2fb6fe4b3dfe789163cccb0fc5',
+        price_old : 150000,
+        price_current : 100000,
+        solded : '100',
+        brand : 'TP',
+        address : 'Nam Định',
+       
+    },
+    {
+        name : 'Áo Polo Nam cổ bẻ TSIMPLE thun basic cộc tay vải cá sấu cotton phong cách trẻ trung chuẩn form nhiều màu',
+        image :'https://cf.shopee.vn/file/292c246006817e208f4491e033a14a4b',
+        price_old : 200000,
+        price_current : 100000,
+        solded : '100',
+        brand : 'TP',
+        address : 'Nam Định',
+       
+    },
+    {
+        name : 'Đầm chữ A đính đá sang trọng, váy cổ vuông tay phồng khoét eo',
+        image :'https://cf.shopee.vn/file/c14d0b131e2a0fc7a02b4890422b61e9',
+        price_old : 650000,
+        price_current : 500000,
+        solded : '100',
+        brand : 'TP',
+        address : 'Nam Định',
+       
+    },
+    {
+        name : 'Váy Cổ Vuông Body, Đầm Công Sở Ôm Dáng Hàn Quốc Sang Trọng SANJOLI Thiết Kế Phù Hợp Dự Tiệc Cưới VD009',
+        image :'https://cf.shopee.vn/file/b4d7b8cec36325179fa6739b4481fe41',
+        price_old : 450000,
+        price_current : 300000,
+        solded : '100',
+        brand : 'TP',
+        address : 'Nam Định',
+       
+    },
+    {
+        name : 'Đầm Cô Dâu Trắng Tay Dài SANJOLI Váy Đi Dự Tiệc Cưới Đẹp, Dáng Suông Maxi Cao Cấp, Thiết Kế Sang Trọng VD029',
+        image :'https://cf.shopee.vn/file/638bd7c066c06417d1eea51ba9e13019',
+        price_old : 550000,
+        price_current : 400000,
+        solded : '100',
+        brand : 'TP',
+        address : 'Nam Định',
+       
+    },
+    {
+        name : 'Đầm Xòe Công Sở Nữ SANJOLI, Váy Đi Dự Tiệc Cưới Thiết Kế Dáng Dài Sang Trọng VD056',
+        image :'https://cf.shopee.vn/file/3cbc0e943a413ee0d0d7ee63ef4fb605',
+        price_old : 500000,
+        price_current : 300000,
+        solded : '100',
+        brand : 'TP',
+        address : 'Nam Định',
+       
+    },
+    {
+        name : 'Set Váy Nữ SANJOLI Set Blazer Nữ Công Sở Thiết Kế Hàn Quốc Tiểu Thư Đi Chơi, Đi Dự Tiệc Cưới AO016',
+        image :'https://cf.shopee.vn/file/fd6201f0b0ad2eddec940777974a79a4',
+        price_old : 750000,
+        price_current : 600000,
+        solded : '100',
+        brand : 'TP',
+        address : 'Nam Định',
+       
+    }
+
+]
+
+
+
+
+
+
+
+
+// header_cart.innerHTML = addCar.join('');
+
+    
+
+
+const itemHTML = items.map((item,index) => {  
+    return `
+    <div class="col l-2-4 m-4 c-6"> 
+        <a class="home__product-item" href="#">
+            <div class="home__product-item__img"
+                style="background-image: url(${item.image});">
+            </div>
+            <h4 class="home__product-item__name">${item.name}</h4>
+            <div class="home__product-item__price">
+                <span class="home__product-item__price-old">${item.price_old}</span>
+                <span class="home__product-item__price-current">${item.price_current}</span>
+            </div>
+            <div class="home__product-item__action">
+                <span class="home__product-item__like">
+                    <i class="home__product-item__like-icon-empty far fa-heart"></i>
+                    <i class="home__product-item__like-icon-fill fas fa-heart"></i>
+                </span>
+                <div class="home__product-item__rating">
+                    <i class="home__product-item__gold fas fa-star"></i>
+                    <i class="home__product-item__gold fas fa-star"></i>
+                    <i class="home__product-item__gold fas fa-star"></i>
+                    <i class="home__product-item__gold fas fa-star"></i>
+                    <i class="fas fa-star"></i>
+                </div>
+                <span class="home__product-item__sold">${item.solded}</span>
+            </div>
+            <div class="home__product-item__origin">
+                <span class="home__product-item__brand">${item.brand}</span>
+                <span class="home__product-item__origin-name">${item.address}</span>
+            </div>
+            <div class="home__product-item__favourite">
+                <i class="fas fa-check"></i>
+                <span>Yêu thích</span>
+            </div>
+            <div class="home__product-item__sale-off">
+                <span class="home__product-item__sale-off-percent">${Math.floor((item.price_old - item.price_current)*100/item.price_old) + '%' }</span>
+                <span class="home__product-item__sale-off-lable">GIẢM</span>
+            </div>
+        </a>
+        <button id="home__product-buy-id" class="home__product-buy">
+            <i class="home__product-buy__icon fas fa-shopping-cart"></i>
+        </button>
+    </div>
+    `
+
+})
+home__product.innerHTML = itemHTML.join('');
+
+
+
+const addCart = function(id,name,img,price){
+    const cartList = {
+        id,
+        name,
+        img,
+        price
+    }
+    console.log(cartList)
+    // const items = arrCart.map(item => {
+    //     return `
+    //     <li class="header__cart-item">
+    //     <img src=${item.img} alt=""
+    //         class="header__cart-item-img">
+    //     <div class="header__cart-item-info">
+    //         <div class="header__cart-item-head">
+    //             <h5 class="header__cart-item-name"> ${item.name} </h5>
+
+    //             </h5>
+    //             <div class="header__cart-item-price-wrap">
+    //                 <span class="header__cart-item-price">${item.price}</span>
+    //                 <span class="header__cart-item-multiply">x</span>
+    //                 <span class="header__cart-item-quatity">1</span>
+    //             </div>
+    //         </div>
+    //         <div class="header__cart-item-body">
+    //             <span class="header__cart-item-description">Phân loại: Bạc</span>
+    //             <span class="header__cart-item-remove">Xóa</span>
+
+    //         </div>
+    //     </div>
+    // </li>
+        
+    //     `
+    // })
+    
+}
